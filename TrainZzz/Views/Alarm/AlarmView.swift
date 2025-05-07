9d16242c705658d359fa16e18961d669d4d8df47
Justin Wong <justinwong124@gmail.com>
2025-05-08 01:17:11 +1000

    J52 - Add spike for API usage

--- TrainZzz/Views/Alarm/AlarmView.swift
+++ TrainZzz/Views/Alarm/AlarmView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct AlarmView: View {
+    @StateObject var stationManager = StationManager()
+    
     var body: some View {
-        Text("I'm an alarm!")
+        SwiftUI.NavigationView {
+            List {
+                ForEach(stationManager.stations) { station in
+                    VStack(alignment: .leading) {
+                        Text(station.name)
+                            .font(.headline)
+                        Text("Lat: \(station.latitude), Lon: \(station.longitude)")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .navigationTitle("Train Stations")
+        }
+        .onAppear {
+            stationManager.fetchStations()
+        }
     }
 }
 
