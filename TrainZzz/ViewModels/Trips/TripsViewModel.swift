59418e45925237247625fb592b23aac0bc40ec94
Lachlan Giang <lachlan.giang@yahoo.com>
2025-05-10 22:54:54 +1000

    LGN - Dropdowns for the stations added

--- TrainZzz/ViewModels/Trips/TripsViewModel.swift
+++ TrainZzz/ViewModels/Trips/TripsViewModel.swift
@@ -2,45 +2,13 @@ import Foundation
 import SwiftUI
 
 class TripsViewModel: ObservableObject {
+    @Published var fromStop: Stop? = nil
+    @Published var toStop: Stop? = nil
     @Published var allStops: [Stop] = []
-    @Published var stops: [Stop] = []
-    @Published var searchText: String = "" {
-        didSet {
-            filterStops()
-        }
-    }
     
-    // Create an index for fast searching
-    var stopNameIndex: [String: [Stop]] = [:]
-
-    init() {
+    init()
+    {
         allStops = loadStops()
-        stops = Array(allStops.prefix(20))
-        buildIndex() // Build the index once on initialization
-    }
-
-    func buildIndex() {
-        // Create an index based on the first 5 characters of the stopName
-        for stop in allStops {
-            let key = stop.stopName.prefix(5).lowercased()
-            stopNameIndex[key, default: []].append(stop)
-        }
-    }
-
-    func filterStops() {
-        if searchText.isEmpty {
-            stops = Array(allStops.prefix(20)) // Default to top 10
-        } else {
-            // Search using the index
-            let key = searchText.prefix(5).lowercased()
-            if let possibleMatches = stopNameIndex[key] {
-                stops = possibleMatches.filter {
-                    $0.stopName.localizedCaseInsensitiveContains(searchText)
-                }
-            } else {
-                stops = [] // No matches
-            }
-        }
     }
     
     func loadStops() -> [Stop] {
--- TrainZzz/Views/Trips/TripsView.swift
+++ TrainZzz/Views/Trips/TripsView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct TripsView: View {
     @StateObject private var viewModel = TripsViewModel()
+    @StateObject private var fromStopVM: StopViewModel
+    @StateObject private var toStopVM: StopViewModel
+
+    init() {
+        let tripsVM = TripsViewModel()
+        _viewModel = StateObject(wrappedValue: tripsVM)
+        _fromStopVM = StateObject(wrappedValue: StopViewModel(allStops: tripsVM.allStops))
+        _toStopVM = StateObject(wrappedValue: StopViewModel(allStops: tripsVM.allStops))
+    }
 
     var body: some View {
-        VStack {
-            Text("Stations")
-            List(viewModel.stops) { stop in
-                VStack(alignment: .leading) {
-                    Text(stop.stopName)
-                        .font(.callout)
+        ZStack {
+            Color(.systemGroupedBackground)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    UIApplication.shared.sendAction(
+                        #selector(UIResponder.resignFirstResponder),
+                        to: nil, from: nil, for: nil
+                    )
                 }
-            }
+            StopSelectView(selectedStop: $viewModel.toStop, viewModel: toStopVM)
+                .offset(y: 250)
+            StopSelectView(selectedStop: $viewModel.fromStop, viewModel: fromStopVM)
+                .offset(y: 100)
         }
     }
 }
