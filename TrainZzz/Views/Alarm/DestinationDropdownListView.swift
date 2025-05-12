0f17052475fb893f4fb245ba3b462cb9a9a37bd1
Justin Wong <justinwong124@gmail.com>
2025-05-13 01:05:19 +1000

    J52 - Update settings configuration for alarm

--- TrainZzz/Views/Alarm/DestinationDropdownListView.swift
+++ TrainZzz/Views/Alarm/DestinationDropdownListView.swift
@@ -5,4 +5,40 @@
 //  Created by Justin Wong on 12/5/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct DestinationDropdownListView: View {
+    var stops: [Stop]
+    var onSelect: (Stop) -> Void
+
+    @Environment(\.colorScheme) var colorScheme
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                ForEach(stops.prefix(10)) { stop in
+                    Button(action: {
+                        onSelect(stop)
+                    }) {
+                        HStack {
+                            Text(stop.stopName)
+                                .foregroundColor(.primary)
+                                .padding(.vertical, 8)
+                                .padding(.horizontal)
+                            Spacer()
+                        }
+                        .background(colorScheme == .dark ? Color(.secondarySystemBackground) : Color(.systemBackground))
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .background(Color(.systemBackground))
+            .cornerRadius(8)
+            .shadow(radius: 5)
+            .padding(.horizontal)
+            .padding(.top)
+        }
+        .frame(maxHeight: 300)
+        .padding(.top)
+    }
+}
