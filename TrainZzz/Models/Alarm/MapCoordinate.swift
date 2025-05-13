3abbc1254a675699a821e010594de412886931c1
Justin Wong <justinwong124@gmail.com>
2025-05-13 20:09:02 +1000

    J52 - Refactor AlarmJourneyTrip with VM and fix calculated properties

---
+++ TrainZzz/Models/Alarm/MapCoordinate.swift
@@ -0,0 +1,8 @@
+//
+//  MapCoordinate.swift
+//  TrainZzz
+//
+//  Created by Justin Wong on 13/5/2025.
+//
+
+import Foundation
---
+++ TrainZzz/ViewModels/Alarm/AlarmJourneyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmJourneyViewModel.swift
+//  TrainZzz
+//
+//  Created by Justin Wong on 13/5/2025.
+//
+
+import Foundation
